563cf791f2166795a2928e6bb09381377c952ad0
Igor Carlos <igor.silva@corebiz.com.br>
2018-02-15 00:47:03 -0200

    activity 1 section 1

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -20,12 +20,10 @@ namespace Vidly
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",  
-                      "~/Content/site.css"));
+                        "~/Content/site.css"));
         }
     }
 }
---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public IList<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer(){ Name = "Igor Carlos", Id = 1},
+                new Customer(){Name = "Testye da silva", Id = 2}
+            };
+
+            return customers;
+        }
+
+
+    }
+
+}
